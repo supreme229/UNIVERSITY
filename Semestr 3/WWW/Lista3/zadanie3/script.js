714e048507cf4d8a7b0485d0d990a9c967489281
supreme229 <315489@uwr.edu.pl>
2021-05-06 23:31:01 +0200

    Backup

--- Semestr 3/WWW/Lista3/zadanie3/script.js
+++
@@ -1,9 +0,0 @@
-var currentBox;
-setInterval(function () {
-  for (var i = 1; i <= 4; i++) {
-    currentBox = document.getElementById(i.toString());
-    setTimeout(function (currentBox) {
-      currentBox.classList.toggle('square-animation');
-    }, i * 200, currentBox)
-  }
-}, 1000)
